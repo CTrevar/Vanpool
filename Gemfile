aa4ca8f754d7ea8a610bd19b340961ad1774bf90
eeveponce <eponce19@gmail.com>
2014-09-30 09:16:02 -0500

    Configuracion de envio de correos

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.13'
 gem 'bcrypt-ruby','3.0.1'
 gem 'faker', '1.0.1'
 gem 'jquery-rails'
+gem 'mail'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "serviciovanpool@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://localhost:3000/signin'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
