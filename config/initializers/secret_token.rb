400a32988ef1e3da0bf8199ce17da56e2dcdd017
eeveponce <eponce19@gmail.com>
2014-10-19 00:13:07 -0500

    Cambia ultimo caracter del secret token de f a d

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '03b6a8a020bac217db13a2fae2001748cbae581eff132a46e32cbc4fdce48c9c93a690fda926d3f9d32a1f96db693d75a3b5b080bc10959b44d25a79efec240f'
+SampleApp::Application.config.secret_token = '03b6a8a020bac217db13a2fae2001748cbae581eff132a46e32cbc4fdce48c9c93a690fda926d3f9d32a1f96db693d75a3b5b080bc10959b44d25a79efec240d'
