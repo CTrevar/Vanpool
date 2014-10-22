8d258738e5edbd0db9a60f805b2488918139c5a9
eeveponce <eponce19@gmail.com>
2014-10-22 01:30:06 -0500

    Agregar tabla de medallas

--- app/controllers/medallas_controller.rb
+++ app/controllers/medallas_controller.rb
@@ -87,4 +87,256 @@ class MedallasController < ApplicationController
     end
   end
 
+   # Método para crear un conductor a través de la forma del panel lateral.
+  # POST /conductors
+  # POST /conductors.json
+  def create
+    paramsmedalla = OpenStruct.new params[:medalla]
+    #paramsuser = OpenStruct.new paramsconductor.user
+    @medalla = Medalla.new
+    #user = User.new
+    @medalla.attributes = {:nombre => paramsmedalla.nombre, 
+                          :puntos => paramsmedalla.puntos, 
+                          :tipomedalla_id => paramsmedalla.tipomedalla_id, 
+                          :imagen => paramsmedalla.imagen, 
+                          :estatus => paramsmedalla.estatus, 
+                          :descripcion => paramsmedalla.descripcion
+                       } 
+    #@conductor.user = user
+    @action = 'create'
+    if @medalla.valid?
+        if @medalla.save!
+          respond_to do |format|
+            # format.js { render :js => "window.location = '#{forma_detalle_conductor @conductor}'" }
+            format.html { render partial: 'administradors/form_medalla', medalla:@medalla, locals: {exito:true}, create: true}
+          end
+        else
+          respond_to do |format|
+            # format.js { render :js => "window.location = '#{forma_detalle_conductor @conductor}'" }
+            format.html { render partial: 'administradors/form_medalla', medalla:@medalla, create: true }
+            format.json { render json: @medalla.errors, status: :unprocessable_entity }
+          end
+        end
+    else
+      respond_to do |format|
+        # format.js { render :js => "window.location = '#{forma_detalle_conductor @conductor}'" }
+        format.html { render partial: 'administradors/form_conductor', conductor:@conductor, create: true }
+        format.json { render json: @conductor.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # Método para actualizar los datos de un conductor a través dela forma del panel lateral.
+  def update
+    paramsmedalla = OpenStruct.new params[:medalla]
+    @medalla = Medalla.find(paramsmedalla.id)
+    @medalla.attributes = {:nombre => paramsmedalla.nombre, 
+                          :puntos => paramsmedalla.puntos, 
+                          :tipomedalla_id => paramsmedalla.tipomedalla_id, 
+                          :imagen => paramsmedalla.imagen, 
+                          :estatus => paramsmedalla.estatus, 
+                          :descripcion => paramsmedalla.descripcion
+                       } 
+    @action = 'update'
+    if @medalla.valid?
+      @medalla.save!
+      respond_to do |format|
+        # format.js { render :js => "window.location = '#{forma_detalle_conductor @conductor}'" }
+        format.html { render partial: 'administradors/form_medalla', medalla:@medalla, locals: {exito:true}}
+      end
+    else
+      respond_to do |format|
+        # format.js { render :js => "window.location = '#{forma_detalle_conductor @conductor}'" }
+        format.html { render partial: 'administradors/form_conductor', medalla:@medalla }
+        format.json { render json: @medalla.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+# =======================================================
+  # =======================================================
+  # Métodos para las jTables
+  # =======================================================
+  # =======================================================
+
+
+  # ///////////////////////////////////////////////////////
+  # Método para listar SIN buscar los registros de la BD
+  #
+  def jtable_list
+
+    jtSorting = params[:jtSorting]
+    jtStartIndex = params[:jtStartIndex]
+    jtPageSize = params[:jtPageSize]
+    jtStartPage = jtStartIndex.to_i / jtPageSize.to_i + 1
+
+    @results = Medalla.order(jtSorting).paginate(page:jtStartPage,per_page:jtPageSize)
+    respond_to do |format|
+      # Regresamos el resultado de la operación a la jTable
+      jTableResult = {:Result => "OK",
+                      :TotalRecordCount => Medalla.count,
+                      :Records => @results}
+      format.json { render json: jTableResult}
+      format.html
+      format.js
+    end
+  end
+
+  # ///////////////////////////////////////////////////////
+  # Método para BUSCAR y listar los registros de la BD
+  #
+  def jtable_filterlist
+    jtTextoBusqueda = params[:textoABuscar]
+    jtSorting = params[:jtSorting]
+    jtStartIndex = params[:jtStartIndex]
+    jtPageSize = params[:jtPageSize]
+    jtStartPage = jtStartIndex.to_i / jtPageSize.to_i + 1
+
+    # Si el campo de busqueda tiene solo espacios en blanco.
+    if jtTextoBusqueda.blank? || jtTextoBusqueda.to_s == ''
+      @results = Medalla.select('*').where("estatus = 't'").order(jtSorting).paginate(page:jtStartPage,per_page:jtPageSize)
+    else
+      # Si contiene algo más realiza la búsqueda en todos los atributos de la tabla.
+      @results = Medalla.select('*').where("(LOWER(nombre) LIKE '%#{jtTextoBusqueda.downcase}%') 
+        AND estatus = 't'").select('*').order(jtSorting).paginate(page:jtStartPage,per_page:jtPageSize)
+    end
+    respond_to do |format|
+      # Regresamos el resultado de la operación a la jTable
+      jTableResult = {:Result => "OK",
+                      :TotalRecordCount => @results.count,
+                      :Records => @results
+      }
+      format.json { render json: jTableResult}
+      format.html
+      format.js
+    end
+  end
+
+  # ///////////////////////////////////////////////////////
+  # Método para eliminar registro de la BD
+  #
+  def jtable_delete
+    medalla = Medalla.find_by_medalla_id(params[:id])
+
+    # Iniciamos la eliminación del registro, si no se elimina, almacenamos el resultado en un boleano.
+    bolExito = true
+    errMensaje = ''
+    begin
+      medalla.estatus = false
+      medalla.save!
+    rescue => e
+      bolExito = false
+      errMensaje = "No se pudo eliminar. Revise el error: #{e}"
+    end
+    respond_to do |format|
+      # Regresamos el resultado de la operación a la jTable
+      if bolExito
+        jTableResult = {:Result => "OK"}
+      else
+        jTableResult = {:Result => "Message",
+                        :Message => errMensaje}
+      end
+      format.json { render json: jTableResult}
+      format.html
+      format.js
+    end
+  end
+
+  # ///////////////////////////////////////////////////////
+  # Método para crear registro en la BD
+  #
+  def jtable_create
+   
+    nombre = params[:nombre]
+    puntos = params[:puntos]
+    tipomedalla_id = params[:tipomedalla_id]
+    imagen = params[:imagen]
+    descripcion = params[:descripcion]
+    
+
+    # Iniciamos la creación del registro, si no se crea, almacenamos el resultado en un boleano.
+    bolexito = false
+    errmensaje = ""
+    begin
+      new = Medalla.new(:nombre => nombre, :puntos => puntos,
+                              :tipomedalla_id => tipomedalla_id, :imagen => imagen,
+                              :descripcion => descripcion)
+      if new.invalid?
+        errmensaje = "No se pudo crear.</br>"
+        errmensaje += new.errors.messages.to_s
+      else
+        new = new.save!
+        bolexito = true
+      end
+    rescue => e
+      errmensaje += "</br>No se pudo crear.</br>Revise el error: </br><em> #{e}</em>"
+    end
+    respond_to do |format|
+      # Regresamos el resultado de la operación a la jTable
+      if bolexito
+        jTableResult = {:Result => "OK",
+                        :Record => new}
+      else
+        jTableResult = {:Result => "Message",
+                        :Message => errmensaje}
+      end
+      format.json { render json: jTableResult}
+      format.html
+      format.js
+    end
+  end
+
+  # ///////////////////////////////////////////////////////
+  # Método para actualizar registro en la BD
+  #
+  def jtable_update
+    # medalla
+     
+    id = params[:id]
+    nombre = params[:nombre]
+    puntos = params[:puntos]
+    tipomedalla_id = params[:tipomedalla_id]
+    imagen = params[:imagen]
+    descripcion = params[:descripcion]
+    estatus = params[:estatus]
+
+
+    # Iniciamos la actualización del registro, si no se acutaliza, almacenamos el resultado en un boleano.
+    bolexito = false
+    errmensaje = ""
+    # begin
+      actualrowmedalla = Medalla.find(id)
+      if actualrowmedalla.present?
+        actualrowmedalla.attributes = { :nombre => nombre, :puntos => puntos,
+         :tipomedalla_id=>tipomedalla_id, :imagen=>imagen,
+          :descripcion=>descripcion, :estatus=>estatus}
+      end
+
+      if actualrowmedalla.valid?
+        actualrowconductor = actualrowconductor.save!
+        bolexito = true
+      else
+        errmensaje = "No se pudo actualizar.</br>"
+        errmensaje += actualrowmedalla.errors.messages.to_s
+      end
+    actualrow = Medalla.select('*').where("id = #{id}")
+    # rescue => e
+    #   errmensaje += "</br>No se pudo actualizar.</br>Revise el error: </br><em> #{e}</em>"
+    # end
+    # Regresamos el resultado de la operación a la jTable
+    respond_to do |format|
+      if bolexito
+        jTableResult = {:Result => "OK",
+                        :Record => actualrow}
+      else
+        jTableResult = {:Result => "Message",
+                        :Message => errmensaje}
+      end
+      format.json { render json: jTableResult}
+      format.html
+      format.js
+    end
+  end
+
 end
