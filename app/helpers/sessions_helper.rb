6cfe3743bec7b5f7671370c7f8178fc943e24bb1
Evelin <eponce19@gmail.com>
2014-11-21 01:38:16 +0000

    Se corrigieron conflictos con merge de Cristy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,31 +1,31 @@
 module SessionsHelper
-	def sign_in(user)
-		cookies.permanent[:remember_token] = user.remember_token
-		self.current_user = user
-	end
+	# def sign_in(user)
+	# 	cookies.permanent[:remember_token] = user.remember_token
+	# 	self.current_user = user
+	# end
 
 	def signed_in?
 		!current_user.nil?
 	end
 
-	def current_user=(user)
-		@current_user = user
-	end
+	# def current_user=(user)
+	# 	@current_user = user
+	# end
 	
-	def current_user
-		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
-	end
+	# def current_user
+	# 	#@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	# end
 
-	def current_user?(user)
-		user == current_user
-	end
+	# def current_user?(user)
+	# 	user == current_user
+	# end
 
-	def signed_in_user
-		unless signed_in?
-			store_location
-			redirect_to signin_path, notice: "Please sign in."
-		end
-	end
+	# def signed_in_user
+	# 	unless signed_in?
+	# 		store_location
+	# 		redirect_to signin_path, notice: "Please sign in."
+	# 	end
+	# end
 
 	def sign_out
 		self.current_user = nil
@@ -33,12 +33,12 @@ module SessionsHelper
 		session.delete(:reservaciones)
 	end
 
-	def redirect_back_or(default)
-		redirect_to(session[:return_to] || default)
-		session.delete(:return_to)
-	end
+	# def redirect_back_or(default)
+	# 	redirect_to(session[:return_to] || default)
+	# 	session.delete(:return_to)
+	# end
 
-	def store_location
-		session[:return_to] = request.fullpath
-	end
+	# def store_location
+	# 	session[:return_to] = request.fullpath
+	# end
 end
