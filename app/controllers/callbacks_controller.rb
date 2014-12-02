077b60757377638334709ba9eee424b96a13b1f4
eeveponce <eponce19@gmail.com>
2014-12-02 16:22:43 -0600

    correccion registro facebook

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -14,30 +14,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
      # redirect_to new_user_registration_url
     #end
 
-    if Cliente.find_by_user_id(@user.id)
-      redirect_to :controller => 'clientes', :action => 'dashboard'
-    else
-      @cliente = Cliente.create(puntaje:0,nivel_id:1,user_id:@user.id)
-      #flash[:success] = "Welcome to the Sample App!"
-      # Tell the UserMailer to send a welcome email after save
-      # UserMailer.welcome_email(@user).deliver
-
-      @openpay=OpenpayApi.new('muvdvkft3dzo57bfzv5g','sk_aa543af9dc964f83b41418a26aa6104f')
-   
-      @customers=@openpay.create(:customers)
-        request_hash={
-          "external_id" => nil,
-          "name" => @cliente.user.name,
-          "last_name" => nil,
-          "email" => @cliente.user.email,
-          "requires_account" => true,
-        }
-        response_hash=@customers.create(request_hash.to_hash)
-        @cliente.openpay_id=response_hash["id"]
-        @cliente.save
-        redirect_to :controller => 'clientes', :action => 'dashboard'
-    end
-
-        
+        sign_in_and_redirect @user
     end
 end
